6a45d6151b80ecfd988e3654fed8dd5ea5197c06
Kylee <ky537465@ucf.edu>
2025-07-30 10:54:21 -0400

    added unit test

---
+++ unit_test.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+import pandas as pd
+from visualizations import load_fifa_data, plot_fifa_data, load_flight_data, plot_flight_data
+
+class TestVisualizations(unittest.TestCase):
+
+    def setUp(self):
+        self.fifa_file = 'fifa_data.csv'
+        self.flight_file = 'flight_data.csv'
+
+        # Make sure the sample data exists
+        self.assertTrue(os.path.exists(self.fifa_file))
+        self.assertTrue(os.path.exists(self.flight_file))
+
+    def test_load_fifa_data(self):
+        data = load_fifa_data(self.fifa_file)
+        self.assertIsInstance(data, pd.DataFrame)
+
+    def test_load_flight_data(self):
+        data = load_flight_data(self.flight_file)
+        self.assertIsInstance(data, pd.DataFrame)
+
+    def test_plot_fifa_data(self):
+        data = load_fifa_data(self.fifa_file)
+        plot_fifa_data(data)
+        self.assertTrue(os.path.exists("fifa_plot.png"))
+
+    def test_plot_flight_data(self):
+        data = load_flight_data(self.flight_file)
+        plot_flight_data(data)
+        self.assertTrue(os.path.exists("spirit_airlines_plot.png"))
+        self.assertTrue(os.path.exists("airline_heatmap.png"))
+
+if __name__ == '__main__':
+    unittest.main()
